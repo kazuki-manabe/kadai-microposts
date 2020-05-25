d2043316f1cd69466a4ace69644a0b9961910d77
kazuki.manabe <beesanzu@icloud.com>
2020-05-25 08:30:22 +0000

    kadai-microposts

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :microposts, only: [:create, :destroy] do
-    member do
-      get :favorite_micropost
-      get :liked
-    end
-  end
+  resources :microposts, only: [:create, :destroy] 
   resources :relationships, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
 end
